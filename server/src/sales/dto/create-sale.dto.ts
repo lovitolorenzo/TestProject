3e868e47fc9fd323335df15a21fd89cb318e553c
Lorenzo <lovitolorenzo@yahoo.it>
2023-02-08 01:21:54 +0100

    fulfills CreateSaleDto class to create a structure for the Data Transfer Object & POST request body

--- server/src/sales/dto/create-sale.dto.ts
+++ server/src/sales/dto/create-sale.dto.ts
@@ -1,1 +1,31 @@
-export class CreateSaleDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateSaleDto {
+	// Swagger decorator to generate swagger documentation
+	@ApiProperty({
+		description: "The ID of the customer associated with the sale",
+		type: Object,
+		example: {
+			connect: {
+				id: "clduuywsg0002yyn7gbo0o3xi",
+			},
+		},
+	})
+	customer!: object;
+
+	@ApiProperty({
+		description: "A list of order IDs associated with the sale",
+		type: Object,
+		example: {
+			connect: [
+				{
+					id: "clduuywsv0006yyn7clapxg0r",
+				},
+			],
+		},
+	})
+	orders!: object;
+
+	@ApiProperty({ description: "The total amount of the sale", type: Number, example: 123.45 })
+	amount?: number;
+}
