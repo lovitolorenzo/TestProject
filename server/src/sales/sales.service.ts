1c17a2c13a4121fdd88fb8330dc273d8c5ecc7d9
Lorenzo <lovitolorenzo@yahoo.it>
2023-02-08 01:24:57 +0100

    makes create(createSaleDto) method in SalesService class able to generate thenew Sale instance in the db

--- server/src/sales/sales.service.ts
+++ server/src/sales/sales.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSaleDto } from './dto/create-sale.dto';
-import { UpdateSaleDto } from './dto/update-sale.dto';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateSaleDto } from "./dto/create-sale.dto";
+import { UpdateSaleDto } from "./dto/update-sale.dto";
 
 @Injectable()
 export class SalesService {
-  create(createSaleDto: CreateSaleDto) {
-    return 'This action adds a new sale';
-  }
+	constructor(private prisma: PrismaService) {}
+	create(createSaleDto: CreateSaleDto) {
+		return this.prisma.sale.create({ data: createSaleDto });
+	}
 
-  findAll() {
-    return `This action returns all sales`;
-  }
+	findAll() {
+		return `This action returns all sales`;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} sale`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} sale`;
+	}
 
-  update(id: number, updateSaleDto: UpdateSaleDto) {
-    return `This action updates a #${id} sale`;
-  }
+	update(id: number, updateSaleDto: UpdateSaleDto) {
+		return `This action updates a #${id} sale`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} sale`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} sale`;
+	}
 }
