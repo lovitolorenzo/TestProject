f5666a58a5e12e8cb4435e4d8f4791e5c9e2cc0f
Lorenzo <lovitolorenzo@yahoo.it>
2023-02-08 01:17:04 +0100

    imports PrismaModule in sales.module to get PrismaService from it

--- server/src/sales/sales.module.ts
+++ server/src/sales/sales.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { SalesService } from './sales.service';
-import { SalesController } from './sales.controller';
+import { Module } from "@nestjs/common";
+import { SalesService } from "./sales.service";
+import { SalesController } from "./sales.controller";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-  controllers: [SalesController],
-  providers: [SalesService]
+	controllers: [SalesController],
+	providers: [SalesService],
+	// Importing PrismaModule which provides PrismaService
+	imports: [PrismaModule],
 })
 export class SalesModule {}
